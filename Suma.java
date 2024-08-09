7e71558759c6544df3bd54e4d537019e73a6b395
AbrahanCC <julioabrhanchetchamale@gmail.com>
2024-08-09 12:39:43 -0600

    Ejemplo suma

---
+++ Suma.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+*Esta clase se encarga de realizar la suma de dos numeros
+*@author Abrahan Chet
+*@since 2024-07-27
+Esta clase fue creada para el ejemplo del curso de Algoritmos
+*/
+Public class Suma{
+
+/*
+*Metodo principal de la clase suma 
+*@param arg parametros introducidos al iniciar el programa
+*@since 2024-07-27
+Estos tags sirven para crear documentacion interna que se puede utilizar para brindar documentos o archivos a los usuarios a cerca de los programas usados, como manuales tecnicos
+*/
+	public static void main (string[]args){
+	
+	//Se declara la variable scan para obtener datos
+	Scanner scan =  new Scanner() System.in);
+
+	int primerNumero = scan.nextInt();
+	int segundoNumero = scan.NextInt ();
+	
+	resultado = primerNumero+segundoNumero;
+		System.println(resultado);
+	}
+}
